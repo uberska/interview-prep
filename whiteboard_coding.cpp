25e6471a4a71beb62e8b76623733323829fb8b6d
Scott Douglas <git@scottdouglas.net>
2013-04-21 19:11:13 -0700

    Added merging of two sorted int arrays into one sorted int array.

--- whiteboard_coding.cpp
+++ whiteboard_coding.cpp
@@ -52,24 +52,114 @@ void reverseString(char* aString) {
 }
 
 
+template <typename DataType>
+void printArray(DataType* array, int arrayCount) {
+    for (int index = 0; index < arrayCount; index++) {
+        if (index != 0) {
+            cout << ", ";
+        }
+        cout << array[index];
+    }
+}
+
+
+void mergeSortedIntArrays(int intArray1[], int array1Count,
+    int intArray2[], int array2Count, int outputArray[]) {
+
+    int array1Index = 0;
+    int array2Index = 0;
+    int outputIndex = 0;
+
+    while (array1Index != array1Count || array2Index != array2Count) {
+        if (array1Index == array1Count) {
+            // Nothing left in intArray1
+            outputArray[outputIndex] = intArray2[array2Index];
+            array2Index++;
+        } else if (array2Index == array2Count) {
+            // Nothing left in intArray2
+            outputArray[outputIndex] = intArray1[array1Index];
+            array1Index++;
+        } else {
+            if (intArray1[array1Index] < intArray2[array2Index]) {
+                outputArray[outputIndex] = intArray1[array1Index];
+                array1Index++;
+            } else {
+                outputArray[outputIndex] = intArray2[array2Index];
+                array2Index++;
+            }
+        }
+
+        outputIndex++;
+    }
+}
+
 
 int main(int argc, char** argv) {
+    //
+    // Let the user know we're starting.
+    //
     cout << endl << "Beginning Whiteboard Coding." << endl;
 
+    //
+    // Reverse a string with an even number of characters
+    //
     printHeader();
     cout << endl << "Reverse String (Even Chars)" << endl;
+
     char* evenString = "Backwards Day (Even)";
+
     cout << "Original: " << evenString << endl;
+
     reverseString(evenString);
+
     cout << "Reversed: " << evenString << endl;
 
+    //
+    // Reverse a string with an odd number of characters
+    //
     printHeader();
     cout << endl << "Reverse String (Odd Chars)" << endl;
+
     char* oddString = "Backwards Day (Odd)";
+
     cout << "Original: " << oddString << endl;
+
     reverseString(oddString);
+
     cout << "Reversed: " << oddString << endl;
 
+    //
+    // Merge two sorted int arrays into one sorted int array
+    //
+    printHeader();
+    cout << endl << "Merge Sorted Int Arrays" << endl;
+
+    int intArray1[] = {1, 3, 6, 8, 10};
+    int array1Count = sizeof(intArray1) / sizeof(intArray1[0]);
+    cout << "Array1  : ";
+    printArray(intArray1, array1Count);
+    cout << endl;
+
+    int intArray2[] = {2, 3, 5, 9, 9};
+    int array2Count = sizeof(intArray2) / sizeof(intArray2[0]);
+    cout << "Array2  : ";
+    printArray(intArray2, array2Count);
+    cout << endl;
+
+    int outputArray[array1Count + array2Count];
+
+    cout << "Expected: 1, 2, 3, 3, 5, 6, 8, 9, 9, 10" << endl;
+
+    mergeSortedIntArrays(intArray1, array1Count, intArray2, array2Count,
+        outputArray);
+    cout << "Actual  : ";
+    printArray(outputArray, array1Count + array2Count);
+
+    cout << endl;
+
+    //
+    // Let the user know we're done.
+    //
     printHeader();
     cout << endl << "Done With Whiteboard Coding." << endl;
 }
